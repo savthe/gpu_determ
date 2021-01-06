2137d437ce245cba2e27eac87fc3dd7b92972c33
savthe <savthe@gmail.com>
2021-01-06 19:57:50 +0300

    cleaning

--- velocity_grid.cu
+++ velocity_grid.cu
@@ -3,80 +3,70 @@
 #include "velocity.h"
 
 
-bool check_if_point_is_in_domain (IntVector n_points, 
-			                     DVector v_min, 
-			                     DVector v_max,
-			                     int i, int j, int k,
-			                     float R)
+bool check_if_point_is_in_domain (IntVector n_points, DVector v_min, DVector v_max,
+			                     int i, int j, int k, float R)
 {
-  float u = (v_max.x - v_min.x) * (2 * i + 1 - n_points.x) / 2. / 
-              n_points.x; 
-  float v = (v_max.y - v_min.y) * (2 * j + 1 - n_points.y) / 2. / 
-              n_points.y; 
-  float w = (v_max.z - v_min.z) * (2 * k + 1 - n_points.z) / 2. / 
-              n_points.z; 
-  return (R <= 0 || u * u + v * v + w * w < R * R);
+	float u = (v_max.x - v_min.x) * (2*i + 1 - n_points.x) / 2. / n_points.x; 
+	float v = (v_max.y - v_min.y) * (2*j + 1 - n_points.y) / 2. / n_points.y;
+	float w = (v_max.z - v_min.z) * (2*k + 1 - n_points.z) / 2. / n_points.z; 
+
+	return R <= 0 || u*u + v*v + w*w < R*R;
 }
 
 
 void VelocityGrid::init(IntVector n_points, DVector v_min, DVector v_max, float R)
 {
-  int i, j, k, index, index1;
-  n_pnt = 0;
   
-  assert (n_points.x > 0 && n_points.y > 0 && n_points.z > 0);
-  assert (v_min.x < v_max.x && v_min.y < v_max.y && v_min.z < v_max.z);
+	assert (n_points.x > 0 && n_points.y > 0 && n_points.z > 0);
+	assert (v_min.x < v_max.x && v_min.y < v_max.y && v_min.z < v_max.z);
  
-  for (i = 0; i < n_points.x; i++)
-    for (j = 0; j < n_points.y; j++)
-      for (k = 0; k < n_points.z; k++)
-	if (check_if_point_is_in_domain (n_points, v_min, v_max, i, j, k, R))
-	  n_pnt++;
+	n_pnt = 0;
+	for (int i = 0; i < n_points.x; i++)
+	for (int j = 0; j < n_points.y; j++)
+	for (int k = 0; k < n_points.z; k++)
+		n_pnt += check_if_point_is_in_domain (n_points, v_min, v_max, i, j, k, R);
   
-  n_u = n_points.x;
-  n_v = n_points.y;
-  n_w = n_points.z;
-  n_uvw = n_points.x * n_points.y * n_points.z;
+	n_u = n_points.x;
+	n_v = n_points.y;
+	n_w = n_points.z;
+	n_uvw = n_points.x * n_points.y * n_points.z;
 
-  
-  u = (float*) malloc(n_u*sizeof(float));
-  v = (float*) malloc(n_v*sizeof(float));
-  w = (float*) malloc(n_points.z*sizeof(float));
+	u = new float[n_u];
+	v = new float[n_v];
+	w = new float[n_w];
 
-  u_index = (int*) malloc(n_pnt*sizeof(int));
-  v_index = (int*) malloc(n_pnt*sizeof(int));
-  w_index = (int*) malloc(n_pnt*sizeof(int));
+	u_index = new int[n_pnt];
+	v_index = new int[n_pnt];
+	w_index = new int[n_pnt];
 
+	min_u = v_min.x;
+	min_v = v_min.y;
+	min_w = v_min.z;
+	max_u = v_max.x;
+	max_v = v_max.y;
+	max_w = v_max.z;
 
-  min_u = v_min.x;
-  min_v = v_min.y;
-  min_w = v_min.z;
-  max_u = v_max.x;
-  max_v = v_max.y;
-  max_w = v_max.z;
-  d_u = (max_u - min_u) / n_u;//N_U (*int_arr);
-  d_v = (max_v - min_v) / n_v;//N_V (*int_arr);
-  d_w = (max_w - min_w) / n_w;//N_W (*int_arr);
-  d3v = d_u*d_v*d_w;
+	d_u = (max_u - min_u) / n_u;//N_U (*int_arr);
+	d_v = (max_v - min_v) / n_v;//N_V (*int_arr);
+	d_w = (max_w - min_w) / n_w;//N_W (*int_arr);
+
+	d3v = d_u*d_v*d_w;
   
   
-  for (i = 0; i < n_u; i++)
-    u[i] = min_u + d_u * (2.*i+1.)/2.;
-  for (i = 0; i < n_v; i++)
-    v[i] = min_v + d_v * (2.*i+1.)/2.;
-  for (i = 0; i < n_w; i++)
-    w[i] = min_w + d_w * (2.*i+1.)/2.;
-  
-  index = 0;
-  for (i = 0, index1 = 0; i < n_u; i++)
-    for (j = 0; j < n_v; j++)
-      for (k = 0; k < n_w; k++, index1++)
-	    if (check_if_point_is_in_domain (n_points, v_min, v_max, i, j, k, R)) {
-	      u_index[index] = i;
-	      v_index[index] = j;
-		  w_index[index] = k;
-	      index++;
+	for (int i = 0; i < n_u; i++) u[i] = min_u + d_u * (2.*i+1.)/2.;
+	for (int i = 0; i < n_v; i++) v[i] = min_v + d_v * (2.*i+1.)/2.;
+	for (int i = 0; i < n_w; i++) w[i] = min_w + d_w * (2.*i+1.)/2.;
+
+	int index = 0;
+	for (int i = 0; i < n_u; i++)
+	for (int j = 0; j < n_v; j++)
+	for (int k = 0; k < n_w; k++)
+		if (check_if_point_is_in_domain (n_points, v_min, v_max, i, j, k, R)) 
+		{
+			u_index[index] = i;
+			v_index[index] = j;
+			w_index[index] = k;
+			index++;
 	    }
-  
 }
 
